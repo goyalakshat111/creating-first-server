2e91a3616af7b2e0e6ea0b705c8e34dc6e9fc5cf
goyalakshat111 <goyalakshat111@gmail.com>
2022-08-29 02:20:25 +0530

    added a new route

--- server.js
+++ server.js
@@ -11,18 +11,56 @@ http
             if (method === "GET") {
                 res.writeHead(200, { "Content-Type": "text/html" });
                 res.write(toDoList.toString());                      // convert array to string and content responded from server
-            } else if (method === "POST") {
+            }
+            else if (method === "POST") {
                 let body = "";
                 req.on("error", (err) => {                  // when user request , error state is for showing error
                     console.error(err);
-                }).on("data", (chunk) => {
-                    body += chunk;                                     // add chunks to body
-                }).on("end", () => {
-                    body = JSON.parse(body);                  // convert chunk (string format) to json format
-                    console.log("data :", body);
-                });
+                })
+                    .on("data", (chunk) => {
+                        body += chunk;                                     // add chunks to body
+                    })
+                    .on("end", () => {
+                        body = JSON.parse(body);                  // convert chunk (string format) to json format
+                        // console.log("data :", body);
 
-            } else {
+                        // ************Day -27 Adding new routes to server**************
+                        let newToDo = toDoList;     // data is not saved to server , whenever we refresh
+                        newToDo.push(body.item);    // it is back to the the initial state. That's why we
+                        console.log(newToDo);       // use database
+                        res.writeHead(201);
+                    });
+
+            }
+            else if (method === "DELETE") {
+                let body = "";
+                req.on("error", (err) => {
+                    console.error(err);
+                })
+                    .on("data", (chunk) => {
+                        body += chunk;
+                    })
+                    .on("end", () => {
+                        body = JSON.parse(body);
+                        let deleteThis = body.item;
+
+                        for (let i = 0; i < toDoList.length; i++) {
+                            if (toDoList[i] === deleteThis) {
+                                toDoList.splice(i, 1);
+                                break;
+                            }
+                        }
+
+                        // toDoList.find((element, index) => {
+                        //     if (element === deleteThis) {
+                        //       toDoList.splice(index, 1);
+                        //     }
+                        //   });
+                        res.writeHead(204);
+                    });
+
+            }
+            else {
                 res.writeHead(501);                      // give error
             }
         } else {
